a7907d685e56948d788ddf87ab6c7f49c6ea4637
jsavinong <j.savinong@gmail.com>
2025-01-03 02:08:01 -0400

    Cambiado todo al idioma español. Inicio de proceso multilenguaje

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx(), sitemap()],
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
